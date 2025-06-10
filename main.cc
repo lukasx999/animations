e73b0a3cb35dde47f00c841fbb58ef73508db872
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-06-10 17:06:56 +0200

    collision fading effect

--- main.cc
+++ main.cc
@@ -13,23 +13,31 @@ protected:
     float m_duration;
 
 public:
-    Animation(float duration) : m_duration(duration) { }
+    Animation(float duration)
+        : m_duration(duration)
+    { }
 
     void start() {
         m_active = true;
     }
 
+    [[nodiscard]] bool is_active() const {
+        return m_active;
+    }
+
     float poll() {
 
         if (m_active) {
+
+            float x =  interpolate();
             m_t += GetFrameTime();
 
-            if (m_t >= m_duration) {
+            if (m_t > m_duration) {
                 m_t = 0;
                 m_active = false;
             }
 
-            return interpolate();
+            return x;
 
         } else {
             return 0;
@@ -44,32 +52,29 @@ protected:
 
 class LinearAnimation : public Animation {
 public:
+
     LinearAnimation(float duration) : Animation(duration) { }
 
 protected:
     [[nodiscard]] float interpolate() const {
-        return Lerp(0, 1, m_t);
+        return m_t / m_duration;
     }
 
 };
 
-class SinAnimation : public Animation {
+class SinAnimation : public LinearAnimation {
     float m_sin_x;
 
 public:
-    SinAnimation(float duration, float sin_x)
-        : Animation(duration)
+
+    SinAnimation(float duration, float sin_x=PI*2)
+        : LinearAnimation(duration)
         , m_sin_x(sin_x)
     { }
 
 protected:
     [[nodiscard]] float interpolate() const {
-        float max = 0;
-        while (max < m_duration)
-            max += GetFrameTime();
-
-        float factor = m_sin_x / max;
-        return Lerp(0, 1, sin(m_t*factor));
+        return sin(m_sin_x * LinearAnimation::interpolate());
     }
 
 };
@@ -82,7 +87,7 @@ int main() {
     raylib::Window window(WIDTH, HEIGHT, "anim", FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(60);
 
-    SinAnimation anim(0.3, PI);
+    SinAnimation anim(1, PI/2);
 
     while (!window.ShouldClose()) {
         BeginDrawing();
@@ -92,28 +97,26 @@ int main() {
 
             float a = anim.poll();
             int size = 100;
-            int outer_size = size + a * 50;
+            int size_diff = a * 100;
+            int outer_size = size + size_diff;
 
-            float speed = 3;
+            float speed = 2;
             double interp = (sin(GetTime() * speed) + 1) / 2;
 
             auto eps = 1e-2;
-
-            if (1 - interp <= eps)
+            if (interp <= eps || 1 - interp <= eps) {
                 anim.start();
-
-            if (interp <= eps)
-                anim.start();
-
+            }
 
             raylib::Rectangle outer(
-                (WIDTH-outer_size) * interp,
+                (WIDTH-size) * interp - size_diff/2.0,
                 HEIGHT / 2.0 - outer_size / 2.0,
                 outer_size,
                 outer_size
             );
-            Color c = RED;
-            c.a *= a;
+
+            Color c = PURPLE;
+            c.a = Lerp(200, 0, a);
             outer.Draw(c);
 
             raylib::Rectangle rect(
